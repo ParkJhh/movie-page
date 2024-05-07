df4c45b09069a8916308e8783740a6d0f7f1b1da
ParkJhh <qkrwnsgur145@gmail.com>
2024-05-07 12:48:03 +0900

    #14 axios api added

--- movie-page-vue/src/assets/movie.js
+++ movie-page-vue/src/assets/movie.js
@@ -6,5 +6,51 @@ export default {
     async mainLoad(num){
         let res = await axios.get(`${API_URL}${num}`);
         return res.data.results
-    }
+    },
+    //커뮤니티 삭제
+    async deleteCommunity(){
+        await axios.delete(`/api/community/${id}`);
+    },
+    //커뮤니티 상세 조회
+    async detailCommunity(){
+        await axios.get(`/api/community/${id}`);
+    },
+    //커뮤니티 목록 조회
+    async listCommunity(){
+        await axios.get(`/api/community//list/${page}`);
+    },
+    //커뮤니티 등록
+    async addCommunity(){
+        await axios.post(`/api/community/`);
+    },
+    //커뮤니티 수정
+    async updateCommunity(){
+        await axios.put(`/api/community/${id}`);
+    },
+
+    //로그인
+    async login(){
+        await axios.post(`/api/login/chk`);
+    },
+    //비밀번호 변경
+    async changePassword(){
+        await axios.post(`/api/login/pwChange`);
+    },
+
+    //회원 탈퇴
+    async deleteMember(){
+        await axios.delete(`/api/member/`);
+    },
+    //회원 가입
+    async addMember(){
+        await axios.post(`/api/member/`);
+    },
+    //회원 수정
+    async updateMember(){
+        await axios.put(`/api/member/`);
+    },
+    //회원 사진 변경
+    async PutProfile(){
+        await axios.put(`/api/member/profile`);
+    },
 }
--- movie-page-vue/vue.config.js
+++ movie-page-vue/vue.config.js
@@ -1,5 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  devServer: {
+    proxy: {
+      "/api": {	
+        target: "http://localhost:8080", // 프록시를 설정할 도메인
+        changeOrigin: true,
+      }, 
+    }
+  },
   transpileDependencies: true,
   lintOnSave: false
 })
